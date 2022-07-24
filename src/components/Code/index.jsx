523b68eb06537b6c797a06578af307812dd4291d
John Read <jr@johnpaulread.com>
2022-07-23 23:49:05 -0700

    save

--- src/components/Code/index.jsx
+++ src/components/Code/index.jsx
@@ -7,56 +7,92 @@ const CopyButton = (props) => {
   return <button className={styles.button} {...props} />
 }
 
-/**
- * @todo vet this method / write-up best ways to handle clipboard
- */
-const copyToClipboard = (str) => {
-  const el = document.createElement('textarea')
-  el.value = str
-  el.setAttribute('readonly', '')
-  el.style.position = 'absolute'
-  el.style.left = '-9999px'
-  document.body.appendChild(el)
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
-}
-
-const Code = ({ codeString, language, ...props }) => {
+const Code = ({ codeString, language, file, ...props }) => {
   const [isCopied, setIsCopied] = React.useState(false)
 
   return (
-    <Highlight
-      {...defaultProps}
-      code={codeString}
-      language={language}
-      theme={theme}
+    <div
+      style={{
+        background: '#011627',
+        borderRadius: '0.75rem',
+        marginTop: '2rem',
+        marginBottom: '2rem',
+        padding: '10px',
+        paddingLeft: '1.5rem',
+        paddingRight: '6px'
+      }}
     >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={className}
-          style={{ ...style, padding: '20px', position: 'relative' }}
+      <div style={{ display: 'flex', position: 'relative' }}>
+        <div
+          style={{
+            color: '#9d9d9d',
+            fontFamily: 'Montserrat',
+            fontStyle: 'italic',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
         >
-          <CopyButton
-            onClick={() => {
-              copyToClipboard(codeString)
-              setIsCopied(true)
-              setTimeout(() => setIsCopied(false), 3000)
-            }}
-          >
-            {isCopied ? <i>Copied!</i> : 'Copy'}
-          </CopyButton>
-
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
+          {file && `${file}`}
+        </div>
+        <div style={{ flexGrow: '1' }}></div>
+        <div
+          style={{
+            marginTop: '6px',
+            // paddingLeft: '0.5rem',
+            paddingRight: '20px',
+            paddingTop: '8px',
+            paddingBottom: '0px',
+            textTransform: 'uppercase',
+            fontFamily: 'Montserrat',
+            fontWeight: 'bold',
+            border: 'none', //'1px solid black',
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >{`${language}`}</div>
+      </div>
+      <div
+        style={{
+          overflow: 'auto',
+          background: '#011627',
+          borderRadius: '0.5rem'
+        }}
+      >
+        <Highlight
+          {...defaultProps}
+          code={codeString}
+          language={language}
+          theme={theme}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre
+              className={className}
+              style={{ ...style, position: 'relative', padding: '20px' }}
+            >
+              <CopyButton
+                onClick={() => {
+                  navigator.clipboard.writeText(codeString)
+                  setIsCopied(true)
+                  setTimeout(() => setIsCopied(false), 4000)
+                }}
+              >
+                {isCopied ? <i>Copied</i> : 'Copy'}
+              </CopyButton>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
               ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
+            </pre>
+          )}
+        </Highlight>
+      </div>
+    </div>
   )
 }
 
