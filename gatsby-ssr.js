d3664115cbe52b83d74b3fbc52bede7f1ac59175
John Read <jr@johnpaulread.com>
2022-07-25 18:21:25 -0700

    auto

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,1 @@
-import * as React from 'react'
-import 'normalize.css'
-import './src/styles/global.css'
-import Layout from '@components/Layout'
-
-const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
-
-export { wrapPageElement }
+export { WrapPageElement } from './gatsby-browser.js'
